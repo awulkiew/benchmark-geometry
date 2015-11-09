1be53e92d37bddb5c129adb4fc7cdc674a66389d
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2015-11-09 22:36:11 +0100

    Add info messages to run script.

--- run.sh
+++ run.sh
@@ -11,16 +11,20 @@ mkdir -p results
 
 cd results
 for f in ../benchmarks/*.cpp; do
-	rm -f ../build/benchmark
+    rm -f ../build/benchmark
+    echo 'Compiling $f';
     g++ -O2 -std="c++11" -I$1 $f -o ../build/benchmark
     for i in `seq 1 10`;
         do
+            echo 'Run $i'
             ../build/benchmark >> $3
         done
 	rm -f ../build/benchmark
 done
 
+echo 'Compiling report generator';
 g++ -std="c++11" ../report/report.cpp -o ../build/report
+echo 'Generating report';
 ../build/report $2 $3
 
 rm -f $3
