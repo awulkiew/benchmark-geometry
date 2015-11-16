54048db5904a7dec771203bde4754c33c590d2d5
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2015-11-16 19:07:55 +0100

    Fix error in run_benchmarks script (wrong script argument used).

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -15,7 +15,7 @@ for f in benchmarks/*.cpp; do
     g++ -O2 -std="c++11" -I$1 $f -o build/benchmark
     for i in `seq 1 20`;
         do
-            ./build/benchmark >> temp/$3
+            ./build/benchmark >> temp/$2
         done
 	rm -f build/benchmark
 done
