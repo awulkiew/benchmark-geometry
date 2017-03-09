3561e5fb9fdcc45b246f4e6e7ee9625a0b6bdf73
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2017-03-09 19:51:31 +0100

    Take RESULTS-DIR in run_report script, remove results from master.

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -13,9 +13,12 @@ for f in benchmarks/*.cpp; do
     rm -f build/benchmark
     echo "Benchmarking: $f";
     g++ -O2 -std="c++11" -I$1 $f -o build/benchmark
-    for i in `seq 1 20`;
+
+    for i in `seq 1 10`;
         do
             ./build/benchmark >> temp/$2
+            printf '.'
         done
-	rm -f build/benchmark
+    printf '\n'
+    rm -f build/benchmark
 done
--- run_report.sh
+++ run_report.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
 if [ "$#" -lt 2 ]; then
-	echo 'Intended usage'
-    echo '    run_report TIMESTAMP SHA'
+    echo 'Intended usage'
+    echo '    run_report TIMESTAMP SHA [RESULTS_DIR]'
     exit
 fi
 
-mkdir -p results
+RESULTS_DIR=results
+TEMP_RESULTS_DIR=../results
+
+if [ "$#" -gt 2 ]; then
+    if [[ "$3" = /* ]]; then
+        RESULTS_DIR=$3
+        TEMP_RESULTS_DIR=$3
+    else
+        RESULTS_DIR=$3
+        TEMP_RESULTS_DIR=../$3
+    fi
+fi
+
+mkdir -p $RESULTS_DIR
 
 echo 'Generating report';
 g++ -std="c++11" report/report.cpp -o build/report
-cd temp && ../build/report $1 $2 ../results
+cd temp && ../build/report $1 $2 $TEMP_RESULTS_DIR
